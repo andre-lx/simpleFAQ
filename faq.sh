3b11ed551acbf190ea4a67b8c2cdb7008c695b9d
andre-lx <44682155+andre-lx@users.noreply.github.com>
2019-03-12 14:50:54 +0000

    Update faq.sh

--- faq.sh
+++ faq.sh
@@ -8,14 +8,15 @@ echo -e '\e[32m'
 if [ "$1" == "help" ] || [ "$1" == "" ]; then
    echo "		faq help - this view
    		faq SERVICE - view service faq
+                faq SERVICE new - open new service on vim 
    	 	faq SERVICE edit - open existing service on vim
-   		faq SERVICE new - open new service on vim
+   		faq SERVICE rm - remove service if exists
    	 	faq ls - list service files"
    echo -e '\e[0m'
    exit
 elif [ "$1" == "ls" ]; then
    ls $servicespath/
-   echo -e '\e[0m'
+   echo -e '\e[0'
    exit
 fi
 
@@ -25,6 +26,8 @@ then
       vi $servicespath/$1
    elif [ "$2" == "new" ]; then   
       echo -e "\e[31mService already on faq! \nEdit service with 'faq serviceName edit' \e[0m"
+   elif [ "$2" == "rm" ]; then
+      rm $servicespath/$1
    else
       cat $servicespath/$1
    fi
@@ -35,5 +38,9 @@ else
       echo -e "\e[31mService not found on faq! \nCreate new service with 'faq serviceName new' \e[0m"
    fi
 fi
-echo -e '\e[0m'
 
+echo "
+Available services:"
+ls $servicespath/
+
+echo -e '\e[0m'
